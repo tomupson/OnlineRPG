dfc882ebc0a1dbeb6079367a64ae1c940fa436d4
Tom Upson <30987275+UppyMeister@users.noreply.github.com>
2019-06-16 17:55:08 +0100

    Update Photon

--- OnlineRPG/OnlineRPG/Assets/Scripts/PostProcessingInitializer.cs
+++ OnlineRPG/OnlineRPG/Assets/Scripts/PostProcessingInitializer.cs
@@ -1,44 +1,45 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(PostProcessingBehaviour), typeof(Camera))]
+//[RequireComponent(typeof(PostProcessingBehaviour), typeof(Camera))]
+[RequireComponent(typeof(Camera))]
 public class PostProcessingInitializer : MonoBehaviour
 {
-    PostProcessingBehaviour ppBehaviour;
+    //PostProcessingBehaviour ppBehaviour;
 
     GraphicsManager graphicsMan;
 
     void Start()
     {
-        ppBehaviour = GetComponent<PostProcessingBehaviour>();
+        //ppBehaviour = GetComponent<PostProcessingBehaviour>();
 
         graphicsMan = GraphicsManager.singleton;
 
         // Motion Blur
-        ppBehaviour.profile.motionBlur.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("MOTION_BLUR")).IsChecked;
-        ppBehaviour.profile.motionBlur.settings = new MotionBlurModel.Settings()
-        {
-            shutterAngle = ((float)((SliderInfo)graphicsMan.GetSetting("MOTION_BLUR_AMOUNT")).Value / 100.0f) * 360.0f,
-            frameBlending = MotionBlurModel.Settings.defaultSettings.frameBlending,
-            sampleCount = MotionBlurModel.Settings.defaultSettings.sampleCount
-        };
+        //ppBehaviour.profile.motionBlur.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("MOTION_BLUR")).IsChecked;
+        //ppBehaviour.profile.motionBlur.settings = new MotionBlurModel.Settings()
+        //{
+        //    shutterAngle = ((float)((SliderInfo)graphicsMan.GetSetting("MOTION_BLUR_AMOUNT")).Value / 100.0f) * 360.0f,
+        //    frameBlending = MotionBlurModel.Settings.defaultSettings.frameBlending,
+        //    sampleCount = MotionBlurModel.Settings.defaultSettings.sampleCount
+        //};
 
-        ppBehaviour.profile.ambientOcclusion.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("AMBIENT_OCCLUSION")).IsChecked;
-        ppBehaviour.profile.bloom.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("BLOOM")).IsChecked;
+        //ppBehaviour.profile.ambientOcclusion.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("AMBIENT_OCCLUSION")).IsChecked;
+        //ppBehaviour.profile.bloom.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("BLOOM")).IsChecked;
 
         EventHandler.OnGraphicsSettingsChanged += CheckForChange;
     }
 
     void CheckForChange()
     {
-        ppBehaviour.profile.motionBlur.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("MOTION_BLUR")).IsChecked;
-        ppBehaviour.profile.motionBlur.settings = new MotionBlurModel.Settings()
-        {
-            shutterAngle = ((float)((SliderInfo)graphicsMan.GetSetting("MOTION_BLUR_AMOUNT")).Value / 100.0f) * 360.0f,
-            frameBlending = MotionBlurModel.Settings.defaultSettings.frameBlending,
-            sampleCount = MotionBlurModel.Settings.defaultSettings.sampleCount
-        };
+        //ppBehaviour.profile.motionBlur.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("MOTION_BLUR")).IsChecked;
+        //ppBehaviour.profile.motionBlur.settings = new MotionBlurModel.Settings()
+        //{
+        //    shutterAngle = ((float)((SliderInfo)graphicsMan.GetSetting("MOTION_BLUR_AMOUNT")).Value / 100.0f) * 360.0f,
+        //    frameBlending = MotionBlurModel.Settings.defaultSettings.frameBlending,
+        //    sampleCount = MotionBlurModel.Settings.defaultSettings.sampleCount
+        //};
 
-        ppBehaviour.profile.ambientOcclusion.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("AMBIENT_OCCLUSION")).IsChecked;
-        ppBehaviour.profile.bloom.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("BLOOM")).IsChecked;
+        //ppBehaviour.profile.ambientOcclusion.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("AMBIENT_OCCLUSION")).IsChecked;
+        //ppBehaviour.profile.bloom.enabled = (bool)((ToggleInfo)graphicsMan.GetSetting("BLOOM")).IsChecked;
     }
 }
